91d31a9ec16183c387a40bf828be4a07131cf990
himavarshini <himayarragangu@gmail.com>
2018-01-20 17:45:21 +0000

    JS

---
+++ PracticeLab/hello.js
@@ -0,0 +1,12 @@
+function doSomeThing()
+{
+    alert("This is prob1");
+    var i;
+    for(i=0;i<=15;i++){
+	if(i%2 === 0){
+		console.log(i + " is even");	
+}
+	else
+		console.log(i+ " is odd");
+    }
+}
